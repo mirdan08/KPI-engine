af1715d8403a41c21d3b644c3c0a7182c236ade4
mirdan08 <m.dangelo20@studenti.unipi.it>
2024-11-20 18:06:44 +0100

    added enums and changed logic of KPIEngine.calculate

--- app/KPI_engine/EngineKPI/kpi_engine.py
+++ app/KPI_engine/EngineKPI/kpi_engine.py
@@ -1,6 +1,30 @@
-import pandas as pd
+from enum import Enum
 from KPI_engine.EngineCalculation.calculation_engine import CalculationEngine
 from MOCK_Knowledge_base.knowledge_base_interface import KnowledgeBaseInterface
+
+
+class KPINames(Enum):
+    WORKING_TIME = "working_time"
+    IDLE_TIME = "idle_time"
+    OFFLINE_TIME = "offline_time"
+    CONSUMPTION = "consumption"
+    POWER = "power"
+    COST = "cost"
+    CONSUMPTION_WORKING = "consumption_working"
+    CONSUMPTION_idle = "consumption_idle"
+    CYCLES = "cycles"
+    BAD_CYCLES = "bad_cycles"
+    GOOF_CYCLES = "good_cycles"
+    AVERAGE_CYCLE_TIME = "average_cycle_time"
+
+
+class CalculationCodes(Enum):
+    OK = 0
+    INVALID_VALUES = 1
+    EXPRESSION_ERROR = 2
+    WRONG_DATA = 3
+    GENERIC_ERROR = 4
+
 
 class KPIEngine:
     def __init__(self):
@@ -14,74 +38,65 @@ class KPIEngine:
                   end_time
                   ):
 
-        response = {
-            "result": None,
-        }
-        start_datetime = None
-        end_datetime = None
-        try:
-            start_datetime = pd.to_datetime(start_time)
-            end_datetime = pd.to_datetime(end_time)
-        except (TypeError, ValueError):
-            response["reason"] = "invalid date format"
-            response["code"] = 7
-            response["result"] = None
-            return response
-
+        result = None
+        reason = None
         # query validation
-        if start_datetime > end_datetime:
-            response["reason"] = "start time cannot be before end time\n"
-            response["code"] = 1
-            response["result"] = None
-            return response
+        if start_time > end_time:
+            reason = "start time cannot be before end time\n"
+            code = CalculationCodes.INVALID_VALUES
+            result = None
+            return code, reason, result
         # check machine presence
         if KnowledgeBaseInterface.get_machine(machine_id) is None:
-            response["reason"] = "machine not present in database\n"
-            response["code"] = 2
-            response["result"]=None
-            return response
+            reason = "machine not present in database\n"
+            code = CalculationCodes.WRONG_DATA
+            result = None
+            return code, reason, result
         
         # extract kpis involved
-
+        """
         calculator = CalculationEngine.Calculator(
             name="",
             description="",
             expression=kpi_query,
-            KPIs=CalculationEngine._KPI_names,
-            functions=CalculationEngine._base_functions
+            KPIs=[kpi.value for kpi in KPINames],
+            base_functions=CalculationEngine._base_functions_dict,
+            complex_functions={},
+
         )
         # TODO: integrate inside calculation engine (?)
-        """ expr_kpis = calculator.get_KPIs()
+         expr_kpis = calculator.get_KPIs()
         # check semantic validity of kpis
         if not all(
             KnowledgeBaseInterface.check_validity(machine_id, kpi, operation)
             for kpi in expr_kpis
         ):
-            response["reason"] = "invalid query values"
-            response["code"] = 3
-            response["result"]=None
-            return response """
+            reason = "invalid query values"
+            code = 3
+            result=None
+            return code, reason, result 
         
         # calculate results of query
         result = calculator(
             machine_id, start_time, end_time, operation
         )
+        """
         # check result validity
         if result is None:
-            response["reason"] = "wrong expression or value encountered"
-            response["code"] = 5
-            response["result"]=None
-            return response
+            reason = "wrong expression or value encountered"
+            code = CalculationCodes.GENERIC_ERROR
+            result = None
+            return code, reason, result
         elif isinstance(result, str):
-            response["result"]=None
-            response["reason"]="wrong values in the calculation"
-            response["code"]=6
-            return response
+            result = None
+            reason = "wrong values in the calculation"
+            code = CalculationCodes.GENERIC_ERROR
+            return code, reason, result
         else:
-            response["result"] = result
-            response["code"] = 0
+            result = result
+            code = CalculationCodes.OK
         # failsafe for unseen cases
-        if response["code"] != 0:
-            response["reason"] = "please check parameters to be correct"
-            response["code"] = 4
-        return response
+        if code != CalculationCodes.OK:
+            reason = "please check parameters to be correct"
+            code = CalculationCodes.GENERIC_ERROR
+        return code, reason, result
