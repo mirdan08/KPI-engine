8744a79f514b5538f8cdef36c333b463532802bb
mirdan08 <m.dangelo20@studenti.unipi.it>
2024-11-20 18:06:56 +0100

    added API endpoints

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, Path, status
+from fastapi.responses import JSONResponse
 from KPI_engine.EngineKPI.kpi_engine import KPIEngine
-
+from models import KPI, calculation_request
 app = FastAPI()
 
 """ operation = "sum"
@@ -10,18 +11,62 @@ machine_id = "ast-xpimckaf3dlf"
 expression = "(good_cycles/cycles)" """
 
 
+engine = KPIEngine()
 @app.get("/calculate/")
 def calculate(
-         machine_id: str = None,
-         operation: str = "sum",
-         expression: str = None,
-         start_date: str = None,
-         end_date: str = None
+         request: calculation_request.KPICalculationRequest = Depends()
          ):
-     engine = KPIEngine()
-     return engine.calculate(machine_id,
-                            expression,
-                            operation,
-                            start_date,
-                            end_date
-                            )
+     
+    code, reason, result = engine.calculate(
+        request.machine_id,
+        request.expression,
+        request.operation,
+        request.start_date,
+        request.end_date
+        )
+    status_code = status.HTTP_200_OK
+    return JSONResponse(
+        content={
+            'code': code.value,
+            'reason': reason,
+            'result': result
+        },
+        status_code=status_code
+    )
+
+
+@app.get("/KPI/{KPIID}/machineKPIValues")
+def getMachineKPI(
+                KPIID=Path(),
+                query_params: KPI.MachineKpiValuesRequest = Depends(KPI.MachineKpiValuesRequest)
+            ):
+    response = {}
+    status_code = status.HTTP_200_OK
+    return JSONResponse(content=response, status_code=status_code)
+
+
+@app.get("/KPI")
+def get_kpi():
+    response = {}
+    status_code = status.HTTP_200_OK
+    return JSONResponse(content=response, status_code=status_code)
+
+
+@app.get("/KPI/{KPIName}/machines")
+def get_kpi_machines(KPIName=Path()):
+    response = {}
+    status_code = status.HTTP_200_OK
+    return JSONResponse(content=response, status_code=status_code)
+
+
+@app.get("/kpi/{KPIID}")
+def get_kpi_id(KPIID=Path()):
+    response = {}
+    status_code = status.HTTP_200_OK
+    return JSONResponse(content=response, status_code=status_code)
+
+@app.post("/KPI")
+def getKPI(KPIID=Path()):
+    response = {}
+    status_code = status.HTTP_200_OK
+    return JSONResponse(content=response, status_code=status_code)
